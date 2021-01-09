66daaae47dae165880fd16a8a9d1b0cc2d82469f
kathiresan97 <kathiresanmk97@gmail.com>
2021-01-09 21:13:56 +0530

    hello kathir

---
+++ src/org/hello/Hello.java
@@ -0,0 +1,13 @@
+package org.hello;
+
+public class Hello {
+	private void helloMethod() {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+	}
+	public static void main(String[] args) {
+		Hello h=new Hello();
+		h.helloMethod();
+	}
+
+}
